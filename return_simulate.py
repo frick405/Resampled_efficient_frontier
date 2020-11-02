ab44086eae4a65bb53e3d286696b2643393a757b
frick405 <myoungseok.chae@qraftec.com>
2020-11-03 00:05:10 +0900

    add resampled efficient frontier class

--- return_simulate.py
+++ return_simulate.py
@@ -24,7 +24,7 @@ class Simulation:
 
         rv = sp.stats.multivariate_normal(estimated_mean, estimated_cov)
 
-        x = rv.rvs(select_num)
+        x = rv.
 
         return x
 
@@ -53,10 +53,23 @@ if __name__ == '__main__':
     data_loader = DataLoader()
     ret_df = data_loader.get_sample_return()
 
-    simul = Simulation(ret_df)
-    x = simul.monte_carlo_simulation()
+    simulation = Simulation(ret_df)
+    simul_num = 10
+    x = simulation.monte_carlo_simulation()
+    frontier_y = np.linspace(0, 0.09, 100)
 
-    ef = EfficientFrontier(ret_df, 0.002, 252)
+    for simul in range(simul_num):
+
+        ret_df = pd.DataFrame(x)
+
+        ef = EfficientFrontier(ret_df, 0.002, 252)
+        frontier_x, frontier_y = ef.get_efficient_frontier(ret_df.mean(), ret_df.cov(), frontier_y)
+
+        plt.plot(frontier_x, frontier_y)
+
+    plt.show()
+
+
 
 
 
